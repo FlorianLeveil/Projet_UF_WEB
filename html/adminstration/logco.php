df6c577b0c01bfc2b2ca8c92b364c3e55798b32c
Florian <43409976+FlorianLeveil@users.noreply.github.com>
2019-06-19 17:43:20 +0200

    Update logco.php

--- html/adminstration/logco.php
+++ html/adminstration/logco.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DATABASE_HOST", "localhost");
-define("DATABASE_NAME", "ouijemeco");
-define("DATABASE_USER", "florian");
-define("DATABASE_PASSWORD", "lol123");
+define("DATABASE_HOST", "stevenmehackpaspls");
+define("DATABASE_NAME", "stevenmehackpaspls");
+define("DATABASE_USER", "stevenmehackpaspls");
+define("DATABASE_PASSWORD", "stevenmehackpaspls");
     
 
 $dbl = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.';charset=utf8', DATABASE_USER, DATABASE_PASSWORD);
