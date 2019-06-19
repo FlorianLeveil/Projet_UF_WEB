88ff47f9494c54550cb010397e894bb6d3ab416c
Florian <43409976+FlorianLeveil@users.noreply.github.com>
2019-06-19 17:41:44 +0200

    Update request.php

--- html/request.php
+++ html/request.php
@@ -1,8 +1,8 @@
 <?php 
-define("DATABASE_HOST", "localhost");
-define("DATABASE_NAME", "monsite");
-define("DATABASE_USER", "florian");
-define("DATABASE_PASSWORD", "lol123");
+define("DATABASE_HOST", "stevenmehackpaspls");
+define("DATABASE_NAME", "stevenmehackpaspls");
+define("DATABASE_USER", "stevenmehackpaspls");
+define("DATABASE_PASSWORD", "stevenmehackpaspls");
     
 
 $db = new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME.';charset=utf8', DATABASE_USER, DATABASE_PASSWORD);
